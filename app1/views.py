9c3e8a28d6696a7e506854d24f1d7e5515019bfb
Seizan Shimazaki <seizans@gmail.com>
2013-09-06 16:04:43 +0900

    twitterログインした場合にタイムラインをprintするように実装する

--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,7 @@
+# encoding=utf-8
 from django.shortcuts import render_to_response
+import twitter
+import allauth.socialaccount.models as m
 
 
 def page1(request):
@@ -6,4 +9,20 @@ def page1(request):
         print request.user.profile.height
     else:
         print 'does not have profile'
+    return render_to_response('app1/page1.html')
+
+
+def tweet(request):
+    account = request.user.socialaccount_set.get()
+    app = m.SocialApp.objects.get(provider='twitter')
+    token = m.SocialToken.objects.get(app=app, account=account)
+    api = twitter.Api(
+        consumer_key=app.client_id,
+        consumer_secret=app.secret,
+        access_token_key=token.token,
+        access_token_secret=token.token_secret,
+    )
+    status = api.GetUserTimeline(user_id=account.uid)
+    for s in status:
+        print s.text
     return render_to_response('app1/page1.html')
--- bacon/urls.py
+++ bacon/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,6 +10,8 @@ urlpatterns = patterns(
     # url(r'^bacon/', include('bacon.foo.urls')),
     url(r'^app1/', include('app1.urls')),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/profile/',
+        TemplateView.as_view(template_name='profile.html')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
